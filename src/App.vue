f463b08b0d97c38ab8605e419d0ee3f2c900834d
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-03-14 22:26:51 -0400

    font-awesome setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="flex flex-col min-h-screen">
+        <header>
+          <router-link to="/profile">
+          <i class="fas fa-user"></i>
+          </router-link>
+    </header>
+    <main>
+    <router-view />
+    </main>
+    <footer></footer>
   </div>
-  <router-view/>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
